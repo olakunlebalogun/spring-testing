700eff164eecf1a08c20a34c263ec7e931f52f3a
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-05-14 13:45:30 +0100

    adding test classes to the UUID generated branch

---
+++ src/test/java/com/olakunle/service/implementation/ProductServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.olakunle.service.implementation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void listAllProducts() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void productUpdate() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+}
